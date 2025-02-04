38fd4dd842904b44c11cb46c63883d9808c52259
Joshua Shane Martin <josmar52789@gmail.com>
2025-02-03 22:18:58 -0500

    Initial Commit

---
+++ Vote/VoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoteApp.swift
+//  Vote
+//
+//  Created by Joshua Shane Martin on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct VoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VoteTests/VoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  VoteTests.swift
+//  VoteTests
+//
+//  Created by Joshua Shane Martin on 2/3/25.
+//
+
+import Testing
+@testable import Vote
+
+struct VoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
